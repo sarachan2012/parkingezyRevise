dea1eb1dbd06e10509ccfde57d1f1dde39db1ff0
Sara Chan <sarachan.2012@sis.smu.edu.sg>
2016-04-03 21:25:03 +0800

    forget to save home.controller.js

--- public/home/home.controller.js
+++ public/home/home.controller.js
@@ -27,23 +27,23 @@ angular.module('parkingEzy')
         }
     }, 1000);
 
-    function updateCarparkAvailability() {
-        // possible random number generated is from -5 to 5
-        angular.forEach(malls, function(mall){
-            var id = mall.$id;
-            var curr_lots = mall.Lots;
-            var rand_num = Math.floor(Math.random() * 10) - 5;
-            var new_lots_num = curr_lots + rand_num;
-            if (new_lots_num < 0){
-                new_lots_num = 0;
-            }else if (new_lots_num == 0){
-                new_lots_num = Math.floor((Math.random() * 10) + 1);
-            }
-            Malls.updateMallField(id, 'Lots', new_lots_num);
-        });
-    }
-    // update every 5 seconds
-    $interval(updateCarparkAvailability, 1000 * 5);
+    // function updateCarparkAvailability() {
+    //     // possible random number generated is from -5 to 5
+    //     angular.forEach(malls, function(mall){
+    //         var id = mall.$id;
+    //         var curr_lots = mall.Lots;
+    //         var rand_num = Math.floor(Math.random() * 10) - 5;
+    //         var new_lots_num = curr_lots + rand_num;
+    //         if (new_lots_num < 0){
+    //             new_lots_num = 0;
+    //         }else if (new_lots_num == 0){
+    //             new_lots_num = Math.floor((Math.random() * 10) + 1);
+    //         }
+    //         Malls.updateMallField(id, 'Lots', new_lots_num);
+    //     });
+    // }
+    // // update every 5 seconds
+    // $interval(updateCarparkAvailability, 1000 * 5);
     /**
      * @name: listOfHours, listOfMinutes
      * @type: variable
