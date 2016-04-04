63e03c8d169a8b1aa830151944ca6efcab43d07b
Sara Chan <sarachan.2012@sis.smu.edu.sg>
2016-04-04 17:18:38 +0800

    working lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,20 +1,8 @@
-// dependencies
-var Firebase = require('firebase');
-var FirebaseUrl = "https://finalparkingezy.firebaseio.com/";
-var mallRef = new Firebase(FirebaseUrl);
-
-var async = require('async');
-var AWS = require('aws-sdk');
-var util = require('util');
-
 exports.handler = function(event, context) {
 
-    function updateMallField(mallId, fieldName, value){
-        return mallRef.child(mallId).child(fieldName).set(value);
-    }
     var id = event.id;
     var avgduration = event.avgduration;
-
+    var new_lots_num = event.new_lots_num;
     var waittime = 0;
     var inverseLots = 1;
 
@@ -25,5 +13,5 @@ exports.handler = function(event, context) {
     waittime = Math.round(avgduration * inverseLots);
 
     // return to the results to the application
-   context.succeed({'id' : id, 'waittime': waittime});
+    context.succeed({'id' : id, 'waittime': waittime});
 };
