2ea95cb8c6e472c727e3e08ac1e8211fb1c55678
Sara Chan <sarachan.2012@sis.smu.edu.sg>
2016-04-04 15:15:43 +0800

    new updates from jack

--- public/load/load.controller.js
+++ public/load/load.controller.js
@@ -18,17 +18,31 @@ angular.module('parkingEzy')
             }else if (new_lots_num == 0){
                 new_lots_num = Math.floor((Math.random() * 10) + 1);
             }
-            Malls.updateMallField(id, 'Lots', new_lots_num);
-
-            var curr_footfall = mall.Footfall;
-            var rand_num = Math.floor(Math.random() * 50) - 10;
-            var new_footfall_num = curr_lots + rand_num;
-            if (new_footfall_num < 0){
-                new_footfall_num = 0;
-            }else if (new_footfall_num == 0){
-                new_footfall_num = Math.floor((Math.random() * 10) + 1);
+           
+//            var curr_footfall = mall.Footfall;
+//            var rand_num = Math.floor(Math.random() * 10) - 5;
+//            var new_footfall_num = curr_footfall + rand_num;
+//            if (new_footfall_num < 0 || new_footfall_num == 0){
+//                new_footfall_num = Math.floor((Math.random() * 10) + 12);
+//            }
+            
+            var avgduration = mall.AverageDuration;
+//            var randomLeaveDuration = Math.round(avgduration * ((Math.random(0,2) + Math.random(0,2) + Math.random(0,2) + Math.random(0,2) + Math.random(0,2) + Math.random(0,2)) / 6));
+//            var cars = (new_footfall_num / 100) * 12;
+//            var probabilityOfOneCarLeaving = 1 / cars;
+            var waittime = 0;
+            var inverseLots = 1;
+            if (new_lots_num > 0){
+                inverseLots = 1 / new_lots_num;
             }
-            Malls.updateMallField(id, 'Footfall', new_footfall_num);
+//            var waittime = Math.round(avgduration * probabilityOfOneCarLeaving);
+           // if (new_lots_num < 0 || new_lots_num == 0){
+            waittime = Math.round(avgduration * inverseLots);
+           // }
+            
+            Malls.updateMallField(id, 'Lots', new_lots_num);
+            Malls.updateMallField(id, 'WaitTime', waittime);
+//            Malls.updateMallField(id, 'Footfall', new_footfall_num);
         });
     }
     // update every 5 seconds
